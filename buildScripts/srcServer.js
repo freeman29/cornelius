33acc3a1f2ed83326d4faa17b60ffca13ab772f6
freeman29 <tordafrancis@gmail.com>
2018-01-05 09:04:26 +0800

    add webpack configuration

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import wpConfig from '../webpack.config.dev';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(wpConfig);
+
+app.use(
+    require('webpack-dev-middleware')(compiler, {
+        noInfo: false,
+        publicPath: wpConfig.output.publicPath
+    })
+);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
